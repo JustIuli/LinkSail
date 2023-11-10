9ddd0c8e5b1d9ba2232b01ac7e0f73c35004292a
Iulian Ciubotariu <129002665+JustIuli@users.noreply.github.com>
2023-11-10 11:32:11 +0200

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './styles/app.css';
+import './styles/general.css';
 
 const app = createApp(App);
 
